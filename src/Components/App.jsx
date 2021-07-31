a4958b708e54d91ee3dbe463603bcc861b7de63c
Seiji Matsumoto <seijim27@gmail.com>
2021-07-31 15:12:56 -0400

    Initial setup

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Main } from '../Styles/App.style.js'
+
+const App = () => {
+  return (
+    <Main>
+      Hello World
+    </Main>
+  )
+};
+
+export default App;
